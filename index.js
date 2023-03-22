46824b5897552c32d3c4ad4e9c7c2bd10bdbaf65
charleslamb212 <charlesandrewlamb@gmail.com>
2023-03-22 13:34:06 -0700

    continued logic

--- index.js
+++ index.js
@@ -25,3 +25,65 @@ function handleCellClick(clickedCellEvent) {
   handleCellPlayed(clickedCell, clickedCellIndex); //Calls function to handle cell played
   handleResultValidation(); //Calls function to handle result validation
 }
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer; //Updates the gameState array with the current player
+    clickedCell.innerHTML = currentPlayer; //Updates the HTML with the current player
+    }
+
+    const winningConditions = [ //Array of arrays containing winning conditions for the game
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) { //Loops through the winning conditions array
+            const winCondition = winningConditions[i];
+            let a = gameState[winCondition[0]];
+            let b = gameState[winCondition[1]];
+            let c = gameState[winCondition[2]];
+            if (a === '' || b === '' || c === '') { //If any of the conditions are not met, the loop continues
+                continue;
+            }
+            if (a === b && b === c) { //If any of the conditions are met, the loop breaks
+                roundWon = true;
+                break
+            }
+        }
+
+        if (roundWon) { //If roundWon is true, the game is over and the winning message is displayed
+            statusDisplay.innerHTML = winningMessage();
+            gameActive = false;
+            return;
+        }
+
+        let roundDraw = !gameState.includes(""); //If roundWon is false, the game continues
+        if (roundDraw) { //If roundDraw is true, the game is over and the draw message is displayed
+            statusDisplay.innerHTML = drawMessage();
+            gameActive = false;
+            return;
+        }
+
+        handlePlayerChange(); //If roundDraw is false, the game continues
+    }
+
+    function handlePlayerChange() {
+        currentPlayer = currentPlayer === "X" ? "O" : "X"; //Changes the current player
+        statusDisplay.innerHTML = currentPlayerTurn(); //Displays whose turn it is
+    }
+
+    function handleRestartGame() {
+        gameActive = true; //Sets gameActive to true
+        currentPlayer = "X"; //Sets currentPlayer to "X"
+        gameState = ["", "", "", "", "", "", "", "", ""]; //Resets the gameState array
+        statusDisplay.innerHTML = currentPlayerTurn(); //Displays whose turn it is
+        document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = ""); //Resets the innerHTML of each cell
+    }
+
